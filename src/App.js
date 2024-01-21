a66a151218db9a9519b17e4bcd13e55092c29813
Paul Rathbone <paulrathbone@btinternet.com>
2024-01-21 17:18:42 +0000

    Card flipping done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
+import './app.css'
 
 function App() {
+  const [flashcards, setFlashcards] =  useState(SAMPLE_FLASHCARDS);
   return (
-    <h1>Hello World</h1>
+    <FlashcardList flashcards={flashcards}/>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'Question 1',
+    answer: 'Answer',
+    options: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4'
+    ]
+  }, 
+
+  {
+    id: 2,
+    question: 'Question 2',
+    answer: 'Answer',
+    options: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4'
+    ]
+  }, 
+
+  {
+    id: 3,
+    question: 'Question 3',
+    answer: 'Answer',
+    options: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4'
+    ]
+  } 
+]
 
 export default App;
---
+++ src/Flashcard.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+export default function Flashcard({ flashcard }) {
+    const [flip, setFlip] = useState(false);
+    return (
+        //className: either set to 'card' or 'card & flip'.
+        //onClick: Flashcard items are inverted.
+        <div 
+            className={`card ${flip ? 'flip' : ''}`}
+            onClick={() => setFlip(!flip)}     
+        >
+            <div className='front'>
+                {flashcard.question}
+                <div className='flashcard-options'>
+                    {flashcard.options.map(option => {
+                        return <div className='flashcard-option'>{option}</div>
+                    })}
+
+                </div>
+            </div>
+                <div className='back'>{flashcard.answer}</div>
+           {/* {flip ? flashcard.answer : flashcard.question}*/}
+        </div>
+    )
+}
