86fb9739e6c8d46d16a7025c7abcc9eb45de03ab
Paul Rathbone <paulrathbone@btinternet.com>
2024-01-22 12:47:48 +0000

    useEffect and decoding text completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './FlashcardList';
-import './app.css'
+import './app.css';
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] =  useState(SAMPLE_FLASHCARDS);
+  
+ /* The `useEffect` hook is used to perform side effects in a functional component. In this case, it is
+ used to make an API call to retrieve a set of flashcards from the Open Trivia Database API. */
+  useEffect(() => {
+    axios
+    .get('https://opentdb.com/api.php?amount=10')
+    .then (res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)), 
+          answer
+        ]
+        return {
+          id: `${index}-${Date.now()}`,
+          //Without decodeString, there's lots of 'weird characters'.
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() -.5)
+        }
+      }))
+      console.log(res.data)
+    })
+  }, []);
+  
+  /**
+   * The `decodeString` function decodes a string that contains HTML entities and returns the decoded
+   * string.
+   * @param str - The `str` parameter is a string that represents an encoded string.
+   * @returns The decoded string.
+   */
+  function decodeString(str) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
   return (
     <FlashcardList flashcards={flashcards}/>
   );
